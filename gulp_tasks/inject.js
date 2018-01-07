0bc716bde3052218085731a9d04052ac6489dbfb
Scott Schafer <scott.schafer@gmail.com>
2018-01-06 22:48:51 -0800

    fixed inject.js error

--- gulp_tasks/inject.js
+++ gulp_tasks/inject.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
 // const wiredep = require('wiredep').stream;
-const angularFilesort = require('gulp-angular-filesort');
 const gulpInject = require('gulp-inject');
 
 const conf = require('../conf/gulp.conf');
@@ -14,8 +13,7 @@ function inject() {
   const injectScripts = gulp.src([
     conf.path.tmp('**/*.js'),
     `!${conf.path.tmp('**/*.spec.js')}`
-  ])
-  .pipe(angularFilesort()).on('error', conf.errorHandler('AngularFilesort'));
+  ]);
 
   const injectOptions = {
     ignorePath: [conf.paths.src, conf.paths.tmp],
@@ -25,7 +23,6 @@ function inject() {
   return gulp.src(conf.path.src('index.html'))
     .pipe(gulpInject(injectStyles, injectOptions))
     .pipe(gulpInject(injectScripts, injectOptions))
-    // .pipe(wiredep(Object.assign({}, conf.wiredep)))
     .pipe(gulp.dest(conf.paths.tmp))
     .pipe(browserSync.stream());
 }
